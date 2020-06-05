fd8fa8b18fb62544becf5117f2dd8d661e60e87e
João Victor Pereir Santos <joaovictorsantoscpv@gmail.com>
2020-06-05 14:12:36 -0300

    add: create structure of components signup page without style

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
 
+import { FiMail, FiUser, FiLock } from 'react-icons/fi';
+import { Container } from './styles';
+
+import { Link } from 'react-router-dom';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+import Footer from '../../components/Footer';
+
 const SignUp: React.FC = () => {
-  return <h1>SignUp</h1>;
+  return (
+    <div>
+      <div>
+        <h1>Realizar cadastro</h1>
+        <Input Icon={FiMail} description="Digite seu e-mail" />
+        <Input Icon={FiUser} description="Digite seu nome" />
+        <Input Icon={FiLock} description="Sua senha secreta" />
+        <Input Icon={FiLock} description="Confirme sua senha secreta" />
+        <Button description="Cadastrar" />
+        <small>
+          <Link to="/">Voltar</Link>
+        </small>
+        <Footer />
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
