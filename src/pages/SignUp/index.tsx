f5e4e5d89f30d1d2d78009961b23a386a6d32978
João Victor Pereir Santos <joaovictorsantoscpv@gmail.com>
2020-06-05 14:34:46 -0300

    add: stylize signup page

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -4,26 +4,31 @@ import { FiMail, FiUser, FiLock } from 'react-icons/fi';
 
 import { Link } from 'react-router-dom';
 
+import { Content, Form } from './styles';
+
+import BackgroundImg from '../../components/BackgroundImg';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 import Footer from '../../components/Footer';
 
 const SignUp: React.FC = () => {
   return (
-    <div>
-      <div>
+    <BackgroundImg>
+      <Content>
         <h1>Realizar cadastro</h1>
-        <Input Icon={FiMail} description="Digite seu e-mail" />
-        <Input Icon={FiUser} description="Digite seu nome" />
-        <Input Icon={FiLock} description="Sua senha secreta" />
-        <Input Icon={FiLock} description="Confirme sua senha secreta" />
-        <Button description="Cadastrar" />
+        <Form>
+          <Input Icon={FiMail} description="Digite seu e-mail" />
+          <Input Icon={FiUser} description="Digite seu nome" />
+          <Input Icon={FiLock} description="Sua senha secreta" />
+          <Input Icon={FiLock} description="Confirme sua senha secreta" />
+          <Button description="Cadastrar" />
+        </Form>
         <small>
           <Link to="/">Voltar</Link>
         </small>
         <Footer />
-      </div>
-    </div>
+      </Content>
+    </BackgroundImg>
   );
 };
 
--- src/pages/SignUp/styles.ts
+++ src/pages/SignUp/styles.ts
@@ -1,3 +1,31 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Content = styled.div`
+  max-width: 520px;
+  width: 100%;
+  padding: 32px;
+  margin: auto auto;
+  color: #fff;
+  text-align: center;
+
+  h1 {
+    margin-bottom: 30px;
+    font-size: 25px;
+  }
+
+  a {
+    text-decoration: none;
+    color: #87868b;
+  }
+`;
+
+export const Form = styled.form`
+  max-width: 320px;
+  width: 100%;
+  margin: auto auto;
+  padding-bottom: 20px;
+
+  button {
+    margin-top: 10px;
+  }
+`;
