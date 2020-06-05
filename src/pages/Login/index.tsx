a5b66d0c4fe1a22fc11c2383b94c6c0c0c7095dc
João Victor Pereir Santos <joaovictorsantoscpv@gmail.com>
2020-06-05 10:16:31 -0300

    add: create structure of components login page without style

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,7 +1,28 @@
 import React from 'react';
 
+import skylabImg from '../../assets/login/skylab_img.svg';
+
 const Login = () => {
-  return <h1>Login</h1>;
+  return (
+    <div>
+      <div>
+        <img src={skylabImg} alt="Logo do Skylab, plataforma da Rocketseat" />
+        <input />
+        <input />
+        <a href="#">Esqueci minha senha</a>
+        <button>Entrar</button>
+        <div>
+          Não tem uma conta? <a href="#">Registre-se</a>
+        </div>
+        <div>ou</div>
+        <button>GitHub</button>
+        <div>
+          <small>Skylab versão 1.1.02</small>
+          <small>Rocketseat 2020 © Todos os direitos reservados</small>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
