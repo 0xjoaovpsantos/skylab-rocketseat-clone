3cd3d8cd45b80bfa06da9da53df0b9d2e286d441
João Victor Pereir Santos <joaovictorsantoscpv@gmail.com>
2020-06-08 15:25:52 -0300

    add: create structure initial of gostack with index.tsx

---
+++ src/pages/GoStack/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const GoStack: React.FC = () => {
+  return <h1>GoStack</h1>;
+};
+
+export default GoStack;
--- src/routes.tsx
+++ src/routes.tsx
@@ -4,13 +4,15 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 import Dashboard from './pages/Dashboard';
+import GoStack from './pages/GoStack';
 
 const Routes = () => {
   return (
     <BrowserRouter>
       <Route component={Login} path="/" exact />
       <Route component={SignUp} path="/signup" />
-      <Route component={Dashboard} path="/dashboard" />
+      <Route component={Dashboard} path="/dashboard" exact />
+      <Route component={GoStack} path="/dashboard/gostack-11" />
     </BrowserRouter>
   );
 };
